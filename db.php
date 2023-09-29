e2d068b20ba6331c0fb36ecb177867c475b7da01
Maksym Dovbeshko <maximdove@gmail.com>
2023-09-29 20:31:16 +0300

    Add Dockerfiles and docker-compose.yml so the app is dockerised

--- db.php
+++ db.php
@@ -2,10 +2,10 @@
 session_start();
 
 $conn = mysqli_connect(
-  'localhost',
+  'db',         // <-- This should be 'db' not 'localhost'
   'root',
   'password123',
   'php_mysql_crud'
-) or die(mysqli_erro($mysqli));
+) or die(mysqli_error($conn));  // <-- Fixed the typo here
 
 ?>
